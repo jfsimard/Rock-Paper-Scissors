20644cf84c05b10c693c79ec354f270da497a400
JF Simard <jfsimard2@gmail.com>
2023-10-19 17:13:57 -0400

    code state before into the logic of the game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+
+function getComputerChoice() {
+    return "paper";
+}
+function playRound(playerSelection, computerSelection) {
+    let lcPlayerSelection = playerSelection.toLowerCase();
+    
+    return `You loose! ${computerSelection} beats ${lcPlayerSelection}`;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
