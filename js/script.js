2682835786877f48b447d920ea5ec94cb497e97c
JF Simard <jfsimard2@gmail.com>
2023-10-25 16:07:25 -0400

    Final code, works except a small prompt glitch

--- js/script.js
+++ js/script.js
@@ -1,79 +1,103 @@
 // David (user) will play aginst HAL 5 games of RPS
 // The one who wins the most games is the big Winner
-/* Algorithm - Pseudocode
 
-- David Initiates the game's first round by making a selection
-- In real time HAL produces a random choice between [rock,paper,scissors] in the toolbox array
-- stores it in memory; 
-- HAL compares the two selections and evaluate who's the winner of that(1,2,3,4,5) round
+const toolBox = ['ROCK','PAPER','SCISSORS'];
+let halScore = 0;
+let davidScore = 0;
+let round = 0;
+const maxRounds = 5;
 
-- HAL Updates the game scoreBoard accordingly;
-- Updates the round counter;
-- Displays a message:
---> showing the selections; 
---> saying who won the round;
---> showing the scoreBoard; 
---> prompts David to enter another selection;
-
-The above are repeated in a loop until it gets to round 5 after which HAL displays a message except:
-
---> Showing final score
---> Declares the winner
---> a choice to start a new game OR say BYE!
-*/
 
 function getHalChoice() {
-    const toolBox = ['rock','paper','scissors'];
     let index = Math.floor(Math.random() * toolBox.length);
     return toolBox[(index)];
 }
-function playRound(davidSelection, halSelection) {
-    let lcDavidSelection = davidSelection.toLowerCase();
-    let winner;
-    // David is 1 HAL is 2
-    console.log(halSelection);
-    if(lcDavidSelection == "rock") {
-        if(halSelection == "scissors") {
-            winner = 1;
-        } else if(halSelection == "paper") {
-            winner = 2;
+function playRound() {
+    let davidSelection;
+    davidSelection = prompt('What is your selection?'); 
+    if(davidSelection === null || davidSelection === "") {
+        resetValues();
+        return;
+    }
+    let ucDavidSelection = davidSelection.toUpperCase();
+        
+    let halSelection = getHalChoice();
+    let winner = 0;
+    let roundResults = {"davidSelection": ucDavidSelection, "halSelection": halSelection, "winner": winner};
+    
+    if(ucDavidSelection == "ROCK") {
+        if(halSelection == "SCISSORS") {
+            roundResults.winner = 1;
+        } else if(halSelection == "PAPER") {
+            roundResults.winner = 2;
         } else {
-            winner = 0;
+            roundResults.winner = 0;
         }       
-    } else if(lcDavidSelection == "paper") {
-        if(halSelection == "rock") {
-            winner = 1;
-        } else if(halSelection == "scissors") {
-            winner = 2;
+    } else if(ucDavidSelection == "PAPER") {
+        if(halSelection == "ROCK") {
+            roundResults.winner = 1;
+        } else if(halSelection == "SCISSORS") {
+            roundResults.winner = 2;
         } else {
-            winner = 0;
+            roundResults.winner = 0;
         }   
-    } else if(lcDavidSelection == "scissors") {
-        if(halSelection === "rock") {
-            winner = 1;
-        } else if(halSelection == "paper") {
-            winner = 2;
+    } else if(ucDavidSelection == "SCISSORS") {
+        if(halSelection === "PAPER") {
+            roundResults.winner = 1;
+        } else if(halSelection == "ROCK") {
+            roundResults.winner = 2;
         } else {
-            winner = 0;
+            roundResults.winner = 0;
         }
     } else {
-        return `${lcDavidSelection} is not a correct choice`;
+        davidSelection = prompt(ucDavidSelection + " is not a valid selection, please try again:");
     }
-    return winner;
-}
-function game() {
-    const selections = ['rock','paper','scissors'];
-    for (let i = 0; i < selections.length; i++) {
-        const el = selections[i];
-        //console.log(selections[i]);
-    }
+    updateScoreBoard(roundResults); 
 }
 
-const davidSelection = "scissors";
-const halSelection = getHalChoice();
 
-//console.log(game());
-//console.log(davidSelection);
-//console.log(getHalChoice());
-console.log(playRound(davidSelection, halSelection));
+function updateScoreBoard(results) {
+    let res = results;
+    let msg = "";
+    if(res.winner === 1) {
+        davidScore += 1;
+        round += 1;
+        msg = `${res.davidSelection} beats ${res.halSelection} David wins round ${round}
+        David: ${davidScore} vs HAL: ${halScore}`;
+        console.log(msg);
+        if( (davidScore > halScore) && (davidScore + halScore === 5) ) {
+            resetValues();
+            console.log("Congratulations David, you beat HAL the not so nice computer!!!");
+        }
+    } else if(res.winner === 2) {
+        halScore += 1;
+        round += 1;
+        msg = `${res.halSelection} beats ${res.davidSelection} Hal wins round ${round}
+        David: ${davidScore} vs HAL: ${halScore}`;
+        console.log(msg);
+        if( (halScore > davidScore) && (halScore + davidScore === 5) ) {
+            resetValues();
+            console.log("Congratulations to HAL the computer!!! David, try again.");
+        }
+    } else {
+        msg = `${res.davidSelection} and ${res.halSelection} we have a tie, let's start over
+        David: ${davidScore} vs HAL: ${halScore}`;
+        console.log(msg);
+        playRound();
+    }
+}
 
+function resetValues() {
+    round = 0;
+    halScore = 0;
+    davidScore = 0;
+}
+
+const playButton = document.querySelector('.play-btn');
+
+playButton.addEventListener('click', () => {
+        for (let round = 1; round <= maxRounds; round++) {
+            playRound();
+        }
+    }
+);
